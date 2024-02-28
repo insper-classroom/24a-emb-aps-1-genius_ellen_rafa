8e1d25b210d1bd91589eed2a1e39328e1796fc3f
dourado-rafa <rafaeldbo@al.insper.edu.br>
2024-02-28 20:23:46 +0000

    Padronizando

--- main/main.c
+++ main/main.c
@@ -9,89 +9,98 @@
 #include "hardware/gpio.h"
 
 const int BUZZER_PIN = 28;
-const int BOTAO_VERMELHO = 21;
-const int BOTAO_VERDE = 27;
-const int BOTAO_AZUL = 17;
-const int BOTAO_AMARELO = 19;
 
-const int LED_VERMELHO = 20;
-const int LED_VERDE = 26;
-const int LED_AZUL = 16;
-const int LED_AMARELO = 18;
+const int BTN_RED_PIN = 21;
+const int LED_RED_PIN = 20;
+volatile int red_pressed = 0;
 
-volatile int vermelho = 0;
-volatile int verde = 0;
-volatile int azul = 0;
-volatile int amarelo = 0;
+const int BTN_YELLOW_PIN = 19;
+const int LED_YELLOW_PIN = 18;
+volatile int yellow_pressed = 0;
 
-void btn_callback(uint gpio, uint32_t events) {
-    if (gpio == BOTAO_VERMELHO){
-        vermelho = 1;
-    } else if (gpio == BOTAO_AMARELO){
-        amarelo = 1;
-    } else if (gpio==BOTAO_AZUL){
-        azul = 1;
-    } else if (gpio==BOTAO_VERDE){
-        verde = 1;
+const int BTN_BLUE_PIN = 17;
+const int LED_BLUE_PIN = 16;
+volatile int blue_pressed = 0;
+
+const int BTN_GREEN_PIN = 27;
+const int LED_GREEN_PIN = 26;
+volatile int green_pressed = 0;
+
+const double time = 0.5;
+
+const int LED_LIST[4] = {LED_RED_PIN, LED_YELLOW_PIN, LED_BLUE_PIN, LED_GREEN_PIN};
+const int BTN_LIST[4] = {BTN_RED_PIN, BTN_YELLOW_PIN, BTN_BLUE_PIN, BTN_GREEN_PIN};
+
+void gpio_callback(uint gpio, uint32_t events) {
+    if (gpio == BTN_RED_PIN){
+        red_pressed = 1;
+    } else if (gpio == BTN_YELLOW_PIN){
+        yellow_pressed = 1;
+    } else if (gpio == BTN_BLUE_PIN){
+        blue_pressed = 1;
+    } else if (gpio == BTN_GREEN_PIN){
+        green_pressed = 1;
     }
 };
 
-void onda(double frequencia, double tempo, double pino){
-    double t = (1/frequencia)*1000000;
-    for (int i=0; i<tempo*frequencia; i++){
-        gpio_put(pino, 1);
+void buzzer(double freq, double duration, int pin){
+    double t = 1000000/freq;
+    for (int i=0; i<duration*freq; i++){
+        gpio_put(pin, 1);
         sleep_us(t/2);
-        gpio_put(pino, 0);
+        gpio_put(pin, 0);
         sleep_us(t/2);
     }
 }
+int BTN_LIST;
+int LED_LIST;
 
 int main() {
-    // mudar seed sempre que o jogo se inicia. o seed pode ser ao ligar a plaquinha
-    // pico sdk
     stdio_init_all();
+
     gpio_init(BUZZER_PIN);
     gpio_set_dir(BUZZER_PIN, GPIO_OUT);
 
-    int leds[4] = {LED_VERMELHO, LED_VERDE, LED_AZUL, LED_AMARELO};
-    int botoes[4] = {BOTAO_VERMELHO, BOTAO_VERDE, BOTAO_AZUL, BOTAO_AMARELO};
-    for (int i=0; i<4; i++){
-        gpio_init(botoes[i]);
-        gpio_set_dir(botoes[i], GPIO_IN);
-        gpio_pull_up(botoes[i]);
+    for (int i=0; i < 4; i++){
+        gpio_init(BTN_LIST[i]);
+        gpio_set_dir(BTN_LIST[i], GPIO_IN);
+        gpio_pull_up(BTN_LIST[i]);
 
-        if (i==0){
-            gpio_set_irq_enabled_with_callback(botoes[i], GPIO_IRQ_EDGE_FALL, true, &btn_callback);
-        } else {
-            gpio_set_irq_enabled(botoes[i], GPIO_IRQ_EDGE_FALL , true);
-        }
+        if (i == 0) gpio_set_irq_enabled_with_callback(BTN_LIST[i], GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
+        else gpio_set_irq_enabled(BTN_LIST[i], GPIO_IRQ_EDGE_FALL , true);
 
-        gpio_init(leds[i]);
-        gpio_set_dir(leds[i], GPIO_OUT);
+        gpio_init(LED_LIST[i]);
+        gpio_set_dir(LED_LIST[i], GPIO_OUT);
     }
 
     while (true) {
-        if (vermelho){
-            gpio_put(LED_VERMELHO,1);
-            onda(250,1,BUZZER_PIN);
-            gpio_put(LED_VERMELHO,0);
+        if (red_pressed){
+            gpio_put(LED_RED_PIN,1);
+            buzzer(250, time, BUZZER_PIN);
+            gpio_put(LED_RED_PIN,0);
 
-            vermelho = 0;
-        } else if(amarelo) {
-            gpio_put(LED_AMARELO,1);
-            onda(500,1,BUZZER_PIN);
-            gpio_put(LED_AMARELO,0);
-            amarelo = 0;
-        } else if(azul){
-            gpio_put(LED_AZUL,1);
-            onda(750,1,BUZZER_PIN);
-            gpio_put(LED_AZUL,0);
-            azul = 0;
-        } else if(verde){
-            gpio_put(LED_VERDE,1);
-            onda(1000,1,BUZZER_PIN);
-            gpio_put(LED_VERDE,0);
-            verde = 0;
+            red_pressed = 0;
+        } 
+        if(yellow_pressed) {
+            gpio_put(LED_YELLOW_PIN,1);
+            buzzer(500, time, BUZZER_PIN);
+            gpio_put(LED_YELLOW_PIN,0);
+
+            yellow_pressed = 0;
+        }
+        if(blue_pressed){
+            gpio_put(LED_BLUE_PIN, 1);
+            buzzer(750, time, BUZZER_PIN);
+            gpio_put(LED_BLUE_PIN, 0);
+
+            blue_pressed = 0;
+        }
+        if(green_pressed){
+            gpio_put(LED_GREEN_PIN, 1);
+            buzzer(1000, time, BUZZER_PIN);
+            gpio_put(LED_GREEN_PIN, 0);
+
+            green_pressed = 0;
         }
         
     }
