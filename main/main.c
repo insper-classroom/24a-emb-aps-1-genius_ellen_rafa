37477412a2e01a9e6235701d269737798f578a62
dourado-rafa <rafaeldbo@al.insper.edu.br>
2024-02-28 22:16:47 +0000

    Inicio do jogo e mostrando sequência

--- main/main.c
+++ main/main.c
@@ -1,38 +1,47 @@
-/**
+/*
  * Copyright (c) 2020 Raspberry Pi (Trading) Ltd.
  *
  * SPDX-License-Identifier: BSD-3-Clause
- */
+*/
 
 #include <stdio.h>
 #include "pico/stdlib.h"
 #include "hardware/gpio.h"
 
 const int BUZZER_PIN = 28;
-const int BTN_START_PIN = 11;
+const int BTN_START_PIN = 15;
 
+const int RED = 0;
 const int BTN_RED_PIN = 21;
 const int LED_RED_PIN = 20;
 volatile int red_pressed = 0;
 
+const int YELLOW = 1;
 const int BTN_YELLOW_PIN = 19;
 const int LED_YELLOW_PIN = 18;
 volatile int yellow_pressed = 0;
 
+const int BLUE = 2;
 const int BTN_BLUE_PIN = 17;
 const int LED_BLUE_PIN = 16;
 volatile int blue_pressed = 0;
 
+const int GREEN = 3;
 const int BTN_GREEN_PIN = 27;
 const int LED_GREEN_PIN = 26;
 volatile int green_pressed = 0;
 
-const double time = 0.5;
-volatile int state = 0;
-int led_index = 0;
-
 const int LED_LIST[4] = { LED_RED_PIN, LED_YELLOW_PIN, LED_BLUE_PIN, LED_GREEN_PIN };
 const int BTN_LIST[4] = { BTN_RED_PIN, BTN_YELLOW_PIN, BTN_BLUE_PIN, BTN_GREEN_PIN };
+const int FREQ_LIST[4] = { 250, 500, 750, 1000 };
+
+const double TIME = 0.5;
+volatile int state = 0;
+int raund = 3;
+int sequence[100] = { 1, 0, 3 };
+int led_index = 0;
+
+// Funções <==========================================================================
 
 void gpio_callback(uint gpio, uint32_t events) {
     if (gpio == BTN_START_PIN) {
@@ -42,14 +51,17 @@ void gpio_callback(uint gpio, uint32_t events) {
         } else {
             state = 0;
         }
-    } else if (gpio == BTN_RED_PIN){
+    } 
+    if (state != 2) {
+        if (gpio == BTN_RED_PIN){
         red_pressed = 1;
-    } else if (gpio == BTN_YELLOW_PIN){
-        yellow_pressed = 1;
-    } else if (gpio == BTN_BLUE_PIN){
-        blue_pressed = 1;
-    } else if (gpio == BTN_GREEN_PIN){
-        green_pressed = 1;
+        } else if (gpio == BTN_YELLOW_PIN){
+            yellow_pressed = 1;
+        } else if (gpio == BTN_BLUE_PIN){
+            blue_pressed = 1;
+        } else if (gpio == BTN_GREEN_PIN){
+            green_pressed = 1;
+        }
     }
 };
 
@@ -63,6 +75,15 @@ void buzzer(double freq, double duration, int pin){
     }
 }
 
+void use_color(int color_id) {
+    gpio_put(LED_LIST[color_id], 1);
+    buzzer(FREQ_LIST[color_id], TIME, BUZZER_PIN);
+    gpio_put(LED_LIST[color_id], 0);
+}
+
+
+// Estados <=========================================================================
+
 void idle_state() {
     gpio_put(LED_LIST[led_index%4], 1);
     sleep_ms(250);
@@ -72,33 +93,29 @@ void idle_state() {
 
 void main_state() {
     if (red_pressed){
-        gpio_put(LED_RED_PIN,1);
-        buzzer(250, time, BUZZER_PIN);
-        gpio_put(LED_RED_PIN,0);
-
+        use_color(RED);
         red_pressed = 0;
     } 
     if (yellow_pressed) {
-        gpio_put(LED_YELLOW_PIN,1);
-        buzzer(500, time, BUZZER_PIN);
-        gpio_put(LED_YELLOW_PIN,0);
-
+        use_color(YELLOW);
         yellow_pressed = 0;
     }
     if (blue_pressed){
-        gpio_put(LED_BLUE_PIN, 1);
-        buzzer(750, time, BUZZER_PIN);
-        gpio_put(LED_BLUE_PIN, 0);
-
+        use_color(BLUE);
         blue_pressed = 0;
     }
     if (green_pressed){
-        gpio_put(LED_GREEN_PIN, 1);
-        buzzer(1000, time, BUZZER_PIN);
-        gpio_put(LED_GREEN_PIN, 0);
-
+        use_color(GREEN);
         green_pressed = 0;
     }
+}
+
+void instruction_state() {
+    sleep_ms(500);
+    for (int i=0; i < raund; i++) {
+        use_color(sequence[i]);
+    }
+    state = 1;
 }
 
 int main() {
@@ -124,12 +141,15 @@ int main() {
     }
 
     while (true) {
-        switch (state) {
+        switch (state) {tados <=========================================================================
             case 0:
                 idle_state();
                 break;
             case 1:
                 main_state();
+                break;
+            case 2:
+                instruction_state();
                 break;
             default:
                 idle_state();
